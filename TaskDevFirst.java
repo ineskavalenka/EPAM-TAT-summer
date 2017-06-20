8dfb17f6f9e3ed819e89779fa933713852dd0214
ineskavalenka <ines.kavalenka@gmail.com>
2017-06-20 21:34:56 +0300

    configured the tabs

--- TaskDevFirst.java
+++ TaskDevFirst.java
@@ -4,18 +4,18 @@
  * required multiplier, so as to obtain a replacement number.
  */
 class TaskDevFirst{
-	public static void main(String[] args) {
-		final int START_COUNT = 0;
-		final int END_COUNT = 100;
-		final int FACTOR = 3;
+  public static void main(String[] args) {
+    final int START_COUNT = 0;
+    final int END_COUNT = 100;
+    final int FACTOR = 3;
 		
-		for (int i = START_COUNT; i <= END_COUNT; i++) {
-			if (i % FACTOR == 0) {
-				int temp = i/FACTOR;
-			  System.out.println(FACTOR+"*"+temp);
-			} else {
-				System.out.println(i);
-			}
-		}
-	}
+    for (int i = START_COUNT; i <= END_COUNT; i++) {
+      if (i % FACTOR == 0) {
+        int temp = i/FACTOR;
+        System.out.println(FACTOR+"*"+temp);
+      } else {
+        System.out.println(i);
+      }
+    }
+  }
 }
