737ac0b8a4ba13418b78eedabc3cb3a93e22aaa2
ineskavalenka <ines.kavalenka@gmail.com>
2017-06-21 12:22:41 +0300

    Tutti-frutti edit

--- TaskDevFirst.java
+++ TaskDevFirst.java
@@ -1,19 +1,22 @@
 /**
- * Prints numbers from 0 to 100. All the numbers that are
- * multiples of 3 are replaced by 3*N, where N is the 
- * required multiplier, so as to obtain a replacement number.
+ * Prints numbers from 0 to 100. 
+ * All the multiples of 3 are replaced by "Tutti",
+ * All the multiples of 5 are replaced by "Frutti",
+ * All the multiples of 15 are replaced by "Tutti-Frutti".
  */
 class TaskDevFirst {
   public static void main(String[] args) {
     final int START_COUNT = 0;
     final int END_COUNT = 100;
-    final int FACTOR = 3;
 		
     for (int i = START_COUNT; i <= END_COUNT; i++) {
-      if (i % FACTOR == 0) {
-        int temp = i/FACTOR;
-        System.out.println(FACTOR + "*" + temp);
-      } else {
+      if (i % 3 == 0 && i % 5 == 0) {
+        System.out.println("Tutti-Frutti");
+      } else if (i % 3 == 0) {
+        System.out.println("Tutti");
+      } else if (i % 5 == 0) {    
+        System.out.println("Frutti");
+      } else { 
         System.out.println(i);
       }
     }
