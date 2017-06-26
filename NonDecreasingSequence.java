34d2efde2b900200343225f032945a7c2c6ce6dc
ineskavalenka <ines.kavalenka@gmail.com>
2017-06-26 16:44:10 +0300

    removed the flag

--- NonDecreasingSequence.java
+++ NonDecreasingSequence.java
@@ -6,21 +6,21 @@ class NonDecreasingSequence {
     Scanner input = new Scanner(System.in);
     String sequence = input.nextLine();
     String sequenceSplit[] = sequence.split(" ");   
-    boolean isNonDecreasing = true;   
     if (sequenceSplit.length < 2)
     {
       System.out.println("Sequence must contain at least two numbers. Program exited.");
     } else {
       try { 
-        for (int i = 0; i < sequenceSplit.length-1; i++) {
+        int i;
+        for (i = 0; i < sequenceSplit.length-1; i++) {
           if (Integer.parseInt(sequenceSplit[i]) > Integer.parseInt(sequenceSplit[i+1])) {
-            isNonDecreasing = false;
-            System.out.println("Sequence is not non-decreasing.");
             break;
           }
-        }  
-        if (isNonDecreasing == true) {
+        } 
+        if (i == sequenceSplit.length-1) {
           System.out.println("Sequence is non-decreasing.");
+        } else {
+          System.out.println("Sequence is not non-decreasing.");
         }
       } catch (Exception e) {
         System.out.println("Incorrect Input. Program exited.");
