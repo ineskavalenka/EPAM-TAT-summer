6031faa8e78103b012b34c1723fa77b04b1e0d40
ineskavalenka <ines.kavalenka@gmail.com>
2017-06-27 13:04:14 +0300

    return

--- NonDecreasingSequence.java
+++ NonDecreasingSequence.java
@@ -6,25 +6,21 @@ class NonDecreasingSequence {
     Scanner input = new Scanner(System.in);
     String sequence = input.nextLine();
     String sequenceSplit[] = sequence.split(" ");   
-    if (sequenceSplit.length < 2)
-    {
-      System.out.println("Sequence must contain at least two numbers. Program exited.");
-    } else {
-      try { 
-        int i;
-        for (i = 0; i < sequenceSplit.length-1; i++) {
-          if (Integer.parseInt(sequenceSplit[i]) > Integer.parseInt(sequenceSplit[i+1])) {
-            break;
-          }
-        } 
-        if (i == sequenceSplit.length-1) {
-          System.out.println("Sequence is non-decreasing.");
-        } else {
-          System.out.println("Sequence is not non-decreasing.");
-        }
-      } catch (Exception e) {
-        System.out.println("Incorrect Input. Program exited.");
+    try {    
+      if (sequenceSplit.length < 2) {
+        throw new IllegalArgumentException();
       }
+      for (int i = 0; i < sequenceSplit.length-1; i++) {
+        if (Integer.parseInt(sequenceSplit[i]) > Integer.parseInt(sequenceSplit[i+1])) {
+          System.out.println("Sequence is not non-decreasing.");
+          return;
+        }
+      } 
+      System.out.println("Sequence is non-decreasing.");
+    } catch (NumberFormatException e) {
+      System.out.println( "Incorrect Input. Program exited." ); 
+    } catch (IllegalArgumentException e) {
+      System.out.println( "Sequence must contain at least two numbers. Program exited." );   
     }
   }
 }
