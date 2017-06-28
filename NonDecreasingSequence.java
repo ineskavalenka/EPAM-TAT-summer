8816c100ea1ae75acc5886bf3317c8e7491d3674
ineskavalenka <ines.kavalenka@gmail.com>
2017-06-28 12:33:14 +0300

    args update

--- NonDecreasingSequence.java
+++ NonDecreasingSequence.java
@@ -2,13 +2,16 @@ import java.util.Scanner;
 
 /**
  * Determines, whether the input sequence of integers is non-decreasing.
+ * The user can enter the sequence as a command-line argument.
+ * If the user doesn't enter the sequence as a command-line argument,
+ * the program requests a sequence from the console.
  */
-class NonDecreasingSequence {
-  public static void main(String[] args) {
-    System.out.println("Enter a sequence of integers separated by spaces.");  
-    Scanner input = new Scanner(System.in);
-    String sequence = input.nextLine();
-    String sequenceSplit[] = sequence.split(" ");   
+class IsNonDecreasing {
+/**
+ * Determines, whether the input sequence of integers is non-decreasing,
+ * and writes the result to the console.
+ */
+  public static void checkIfNonDecreasing (String[] sequenceSplit) {
     try {    
       if (sequenceSplit.length < 2) {
         throw new IllegalArgumentException();
@@ -18,12 +21,27 @@ class NonDecreasingSequence {
           System.out.println("Sequence is not non-decreasing.");
           return;
         }
-      } 
+    } 
       System.out.println("Sequence is non-decreasing.");
     } catch (NumberFormatException e) {
       System.out.println( "Incorrect Input. Program exited." ); 
     } catch (IllegalArgumentException e) {
       System.out.println( "Sequence must contain at least two numbers. Program exited." );   
+    }
+  }
+
+  public static void main(String[] args) {
+    try {    
+      if (args.length == 0) {
+      throw new IllegalArgumentException();      
+      } 
+      checkIfNonDecreasing (args);
+    } catch (IllegalArgumentException e) {
+      Scanner input = new Scanner(System.in);
+      System.out.println("Enter a sequence of integers separated by spaces.");  
+      String sequence = input.nextLine();
+      String sequenceSplit[] = sequence.split(" "); 
+      checkIfNonDecreasing(sequenceSplit);
     }
   }
 }
