9eb83117401ced69c4bc771b87a362dbdd23947e
ineskavalenka <ines.kavalenka@gmail.com>
2017-06-29 22:48:47 +0300

    Update IsNonDecreasing.java

--- IsNonDecreasing.java
+++ IsNonDecreasing.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 /**
  * Determines, whether the input sequence of integers is non-decreasing.
  * The user can enter the sequence as a command-line argument.
@@ -7,44 +5,17 @@ import java.util.Scanner;
  * the program requests a sequence from the console.
  */
 class IsNonDecreasing {
-  /**
-   * Determines, whether the input sequence of integers is non-decreasing,
-   * and writes the result to the console.
-   *
-   * @param sequenceSplit a sequence to be analyzed; in case the sequence
-   *                      has incorrect format, warnings will be written to the console. 
-   */
-  public static void checkIfNonDecreasing (String[] sequenceSplit) {
-    try {    
-      if (sequenceSplit.length < 2) {
-        throw new IllegalArgumentException();
-      }
-      for (int i = 0; i < sequenceSplit.length-1; i++) {
-        if (Integer.parseInt(sequenceSplit[i]) > Integer.parseInt(sequenceSplit[i+1])) {
-          System.out.println("Sequence is not non-decreasing.");
-          return;
-        }
-    } 
-      System.out.println("Sequence is non-decreasing.");
-    } catch (NumberFormatException e) {
-      System.out.println( "Incorrect Input. Program exited." ); 
-    } catch (IllegalArgumentException e) {
-      System.out.println( "Sequence must contain at least two numbers. Program exited." );   
-    }
-  }
-
   public static void main(String[] args) {
     try {    
       if (args.length == 0) {
-      throw new IllegalArgumentException();      
+        throw new IllegalArgumentException();      
       } 
-      checkIfNonDecreasing (args);
+      SequenceAnalyzer analyzer = new SequenceAnalyzer();
+      analyzer.checkIfNonDecreasing(args);
     } catch (IllegalArgumentException e) {
-      Scanner input = new Scanner(System.in);
-      System.out.println("Enter a sequence of integers separated by spaces.");  
-      String sequence = input.nextLine();
-      String sequenceSplit[] = sequence.split(" "); 
-      checkIfNonDecreasing(sequenceSplit);
+      ReadFromConsole reader = new ReadFromConsole();
+      SequenceAnalyzer analyzer = new SequenceAnalyzer();
+      analyzer.checkIfNonDecreasing(reader.readSequence());
     }
   }
 }
