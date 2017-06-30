f729840e447c88ff631ec044d5f1958a0af99da9
ineskavalenka <ines.kavalenka@gmail.com>
2017-06-30 16:05:31 +0300

    Update IsNonDecreasing.java

--- IsNonDecreasing.java
+++ IsNonDecreasing.java
@@ -3,19 +3,39 @@
  * The user can enter the sequence as a command-line argument.
  * If the user doesn't enter the sequence as a command-line argument,
  * the program requests a sequence from the console.
+ * 
+ * An additional task was to request the sequence until we could analyze it.
  */
 class IsNonDecreasing {
   public static void main(String[] args) {
-    try {    
-      if (args.length == 0) {
-        throw new IllegalArgumentException();      
-      } 
-      SequenceAnalyzer analyzer = new SequenceAnalyzer();
-      analyzer.checkIfNonDecreasing(args);
-    } catch (IllegalArgumentException e) {
-      ReadFromConsole reader = new ReadFromConsole();
-      SequenceAnalyzer analyzer = new SequenceAnalyzer();
-      analyzer.checkIfNonDecreasing(reader.readSequence());
+    SequenceAnalyzer analyzer = new SequenceAnalyzer();
+    boolean isNonDecreasing = false;
+    ConsoleReader reader = new ConsoleReader();
+    String[] inputSequence = args;
+    if (args.length == 0) {   
+      inputSequence = reader.readSequence();
     }
+    boolean isRepeatRequired = true;
+    while (isRepeatRequired) {
+      try {  
+        isNonDecreasing = analyzer.checkIfNonDecreasing(inputSequence);
+        //no exceptions => we have result
+        isRepeatRequired = false;
+        if (isNonDecreasing) {
+          System.out.println("Sequence is non-decreasing.");
+        } else {
+          System.out.println("Sequence is not non-decreasing.");
+        }
+      } catch (Exception e) {
+        System.out.println("Incorrect input arguments."); 
+        System.out.println("Sequence must be integer and contain at least two numbers.");
+        if (reader.askRepeat()) {
+          inputSequence = reader.readSequence();
+          isRepeatRequired = true;
+        } else {
+          isRepeatRequired = false;
+        } 
+      }
+    }         
   }
 }
