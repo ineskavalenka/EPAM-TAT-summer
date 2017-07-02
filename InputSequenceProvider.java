a77b85d2ad3c768a7d94c2b5565c407ba1b728e5
ineskavalenka <ines.kavalenka@gmail.com>
2017-07-02 20:26:26 +0300

    Update InputSequenceProvider.java

--- InputSequenceProvider.java
+++ InputSequenceProvider.java
@@ -5,19 +5,17 @@ class InputSequenceProvider {
   /**
    * Gets the next input sequence from command line arguments or from console.
    * If the command line arguments contain information, their content is returned as a sequence,
-   * and the command line arguments are erased. If not, the sequence is requested from the console. 
+   * If not, the sequence is requested from the console. 
    * 
    * @param args command line arguments. 
    * @return     the sequence.
    */
   public String[] provideInputSequence(String[] args) {
-    String[] inputSequence = args;
     if (args.length != 0) { 
-     args = new String[0];
+      return args; 
     } else {
-      ConsoleReader reader = new ConsoleReader();
-      inputSequence = reader.readSequence();
+      ConsoleReader reader = new ConsoleReader();  
+      return reader.readSequence(); 
     }
-    return inputSequence;
   }
 }
