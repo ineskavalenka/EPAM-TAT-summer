ad33f594c1ea4cd287c02eae12a5953cd4cfcff5
ineskavalenka <ines.kavalenka@gmail.com>
2017-07-07 18:51:44 +0300

    spaces

--- Triangle.java
+++ Triangle.java
@@ -18,7 +18,7 @@ class Triangle {
     if (!analyzer.checkIfTriangleSides(sidesSequence)) {
       throw new IllegalArgumentException();
     }
-    for (int i=0; i < NUMBER_OF_SIDES; i++) {
+    for (int i = 0; i < NUMBER_OF_SIDES; i++) {
       sides[i] = Double.parseDouble(sidesSequence[i]); 
     } 
     SortingCenter sorter = new SortingCenter();
