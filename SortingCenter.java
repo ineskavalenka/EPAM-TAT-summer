a40fff5e46713913cdb21711f4bac7cdf02710dc
ineskavalenka <ines.kavalenka@gmail.com>
2017-07-11 12:25:07 +0300

    task dev-7

---
+++ SortingCenter.java
@@ -0,0 +1,25 @@
+/**
+ * A collection of sort methods.
+ */
+class SortingCenter {
+  /**
+   * Nondecreasing selection sort (output[0] is min).
+   */
+  public void selectionSort(double[] inputArray) {
+    for (int i = 0; i < inputArray.length; i++) {
+      int minCount = i; 
+      double minElement = inputArray[i];
+      for (int j = i+1; j < inputArray.length; j++) {
+        if (inputArray[j] < minElement) {
+           minElement = inputArray[j];
+           minCount = j;
+        }
+      }
+      if (i != minCount) {
+        double temp = inputArray[i];
+        inputArray[i] = inputArray[minCount];
+        inputArray[minCount] = temp;
+      }
+    }
+  }
+}
---
+++ Triangle.java
@@ -0,0 +1,36 @@
+/**
+ * Describes triangles with double sides.
+ */
+class Triangle {
+  final int NUMBER_OF_SIDES = 3;
+  private double[] sides = new double[NUMBER_OF_SIDES];
+  
+  /**
+   * Triangle constructor; makes a triangle from the sequence of its sides.
+   * Sides of the created triangle are sorted by nondecreasing, even if the input sequence is not.
+   *
+   * @param sidesSequence a sequence of triangle sides
+   * @throws IllegalArgumentException if the sequence has not three sides.
+   * @throws NumberFormatException if sides are not numbers.
+   */
+  public Triangle(String[] sidesSequence) {
+    SequenceAnalyzer analyzer = new SequenceAnalyzer();
+    if (!analyzer.checkIfTriangleSides(sidesSequence)) {
+      throw new IllegalArgumentException();
+    }
+    for (int i=0; i < NUMBER_OF_SIDES; i++) {
+      sides[i] = Double.parseDouble(sidesSequence[i]); 
+    } 
+    SortingCenter sorter = new SortingCenter();
+    sorter.selectionSort(sides);
+  }
+  
+  /**
+   * Triangle side getter.
+   *
+   * @return the sequence of this.sides.
+   */
+  public double[] getSides() {
+    return sides;
+  }
+}
