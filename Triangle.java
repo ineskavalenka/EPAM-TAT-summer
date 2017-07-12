586c14b871d67e79336edf2c6720c2adbba9f9e6
ineskavalenka <ines.kavalenka@gmail.com>
2017-07-12 14:57:46 +0300

    double (not string) sequence in triangle constructor input

--- Triangle.java
+++ Triangle.java
@@ -13,14 +13,8 @@ class Triangle {
    * @throws IllegalArgumentException if the sequence has not three sides.
    * @throws NumberFormatException if sides are not numbers.
    */
-  public Triangle(String[] sidesSequence) {
-    SequenceAnalyzer analyzer = new SequenceAnalyzer();
-    if (!analyzer.checkIfTriangleSides(sidesSequence)) {
-      throw new IllegalArgumentException();
-    }
-    for (int i=0; i < NUMBER_OF_SIDES; i++) {
-      sides[i] = Double.parseDouble(sidesSequence[i]); 
-    } 
+  public Triangle(double[] sides) {
+     
     SortingCenter sorter = new SortingCenter();
     sorter.selectionSort(sides);
   }
--- WhatTriangle.java
+++ WhatTriangle.java
@@ -3,24 +3,37 @@
  * Outputs the results to the console.
  */
 class WhatTriangle {
+  final static int NUMBER_OF_SIDES = 3;
+  
   public static void main(String[] args) {
     ConsoleReader reader = new ConsoleReader();
     InputSequenceProvider sequenceProvider = new InputSequenceProvider(args);      
     do {  
       try {
         String[] sequence = sequenceProvider.provideInputTriangle();
-        Triangle triangle = new Triangle(sequence);   
-        TriangleAnalyzer analyzer = new TriangleAnalyzer(triangle);
+        
+        SequenceAnalyzer sequenceAnalyzer = new SequenceAnalyzer();
+        if (!sequenceAnalyzer.checkIfTriangleSides(sequence)) {
+          throw new IllegalArgumentException();
+        }
+        
+        double sides[] = new double[NUMBER_OF_SIDES];
+        for (int i=0; i < NUMBER_OF_SIDES; i++) {
+          sides[i] = Double.parseDouble(sequence[i]); 
+        }
+        
+        Triangle triangle = new Triangle(sides);   
+        TriangleAnalyzer triangleAnalyzer = new TriangleAnalyzer(triangle);
         System.out.print("This triangle: ");
-        if(analyzer.checkIfEquilateral()) {
+        if(triangleAnalyzer.checkIfEquilateral()) {
           System.out.println("is equilateral.");
         } else {
-          if (analyzer.checkIfRight()) {
+          if (triangleAnalyzer.checkIfRight()) {
             System.out.println("is right.");
           }
-          if (analyzer.checkIfIsosceles()) {
+          if (triangleAnalyzer.checkIfIsosceles()) {
             System.out.println("is isosceles.");
-          } else if (analyzer.checkIfScalene()) {
+          } else if (triangleAnalyzer.checkIfScalene()) {
             System.out.println("is scalene.");
           }        
         }
