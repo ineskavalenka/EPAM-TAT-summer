47c20b0c664083d8d7f547a71dbdce376ddf3632
ineskavalenka <ines.kavalenka@gmail.com>
2017-07-12 15:07:58 +0300

    exists()

--- Triangle.java
+++ Triangle.java
@@ -14,7 +14,9 @@ class Triangle {
    * @throws NumberFormatException if sides are not numbers.
    */
   public Triangle(double[] sides) {
-     
+    if (!exists(sides)) {
+      throw new IllegalArgumentException();
+    }       
     SortingCenter sorter = new SortingCenter();
     sorter.selectionSort(sides);
   }
@@ -26,5 +28,21 @@ class Triangle {
    */
   public double[] getSides() {
     return sides;
+  }
+  
+  public boolean exists(double[] sides) {
+    if (sides.length != NUMBER_OF_SIDES) {
+      return false;
+    }
+    if ((sides[0] + sides[1] - sides[2] <= 0) || (sides[1] + sides[2] - sides[0] <= 0) || 
+    (sides[0] + sides[2] - sides[1] <= 0)) {
+      return false;
+    }
+    for (int i = 0; i < NUMBER_OF_SIDES; i++) {
+      if (sides[i] <= 0) {
+        return false;
+      }
+    }
+    return true;
   }
 }
