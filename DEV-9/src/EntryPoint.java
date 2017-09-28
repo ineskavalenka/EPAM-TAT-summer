f91525e80a0799190686e58c7b0bfbf4cad10488
ineskavalenka <ines.kavalenka@gmail.com>
2017-09-28 22:26:11 +0300

    no input ex

--- DEV-9/src/EntryPoint.java
+++ DEV-9/src/EntryPoint.java
@@ -11,13 +11,18 @@
  */
 public class EntryPoint {
   public static void main(String[]args) {
-    StringProcessor processor = new StringProcessor();
+    if(args.length<2) {
+      System.out.println("No input arguments. Input arguments must be passed as args[0] and " +
+              "args[1]. Program exited.");
+      return;
+    }
     PairOfStrings inputPair = new PairOfStrings(args[0], args[1]);
 
     System.out.println(inputPair.first);
     System.out.println(inputPair.second);
 
     System.out.println("*");
+    StringProcessor processor = new StringProcessor();
     processor.randomMerge(inputPair);
 
     System.out.println(inputPair.first);
