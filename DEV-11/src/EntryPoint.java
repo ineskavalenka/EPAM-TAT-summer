0b6c10bb07595f29c938cb2949ef20fabe670b7e
ineskavalenka <ines.kavalenka@gmail.com>
2017-10-06 14:36:05 +0300

    multiple char javadoc

--- DEV-11/src/EntryPoint.java
+++ DEV-11/src/EntryPoint.java
@@ -5,13 +5,23 @@ import java.util.HashMap;
  * Converts a cyrillic string into a latin string, according to transliteration rules.
  */
 public class EntryPoint {
+  final static int MAX_SYMBOL_LENGTH_RU = 4;
+  final static int MAX_SYMBOL_LENGTH_EN = 4;
+
   public static void main(String[] args) {
     FileReader fileReader = new FileReader();
     try {
-      HashMap ru_en = fileReader.makeDictionary("ru_en_1.txt");
+      HashMap[] ruEn = new HashMap[MAX_SYMBOL_LENGTH_RU+1];
+      for(int i=1; i<=MAX_SYMBOL_LENGTH_RU; i++) {
+        ruEn[i] = fileReader.makeDictionary("ru_en_" + i + ".txt");
+      }
+      HashMap[] enRu = new HashMap[MAX_SYMBOL_LENGTH_RU+1];
+      for(int i=1; i<=MAX_SYMBOL_LENGTH_EN; i++) {
+        enRu[i] = fileReader.makeDictionary("en_ru_" + i + ".txt");
+      }
       String inputRu = "Поешь еще этих свежих булок. БУЛКИ БУЛКИ ЁЁЁЁ!!!";
       StringProcessor stringProcessor = new StringProcessor();
-      System.out.println(stringProcessor.oneSymbolTransliterate(inputRu,ru_en));
+      System.out.println(stringProcessor.oneSymbolTransliterate(inputRu,ruEn[1]));
     } catch (Exception e) {
       e.printStackTrace();
     }
--- DEV-11/src/StringProcessor.java
+++ DEV-11/src/StringProcessor.java
@@ -27,4 +27,61 @@ public class StringProcessor {
     }
     return output;
   }
+
+  /**
+   * Transliterates a string assuming that language symbols can be more than one char length.
+   *
+   * @param input input string to transliterate
+   * @param dictionaries an array of "dictionary"-hashmaps. dictionaries[N] is a dictionary with
+   *                     N-char-length keys.
+   * @param maxSymbolLength maximum symbol length of input language (in number of chars)
+   * @return the transliterated string.
+   */
+  public String multipleSymbolTransliterate(String input, HashMap[] dictionaries, int
+          maxSymbolLength) {
+    //detectSymbolLength()
+    //output+ (detect symbol)
+    //i+=expectedSymbolLength;
+    //return output
+     return input;
+  };
+
+  /**
+   * Detects the length of the next symbol in a string.
+   * Since some longer symbols begin in the same way as shorter ones,
+   * the method first assumes that a symbol has multiple chars. expectedSymbolLength parameter
+   * indicates the biggest possible length. If there is no symbol with that length, the method
+   * calls itself recursively with the expected length decreased.
+   *
+   * @param inputString the input string
+   * @param i the index of first char of a symbol.
+   * @param expectedSymbolLength expected symbol length.
+   * @param dictionaries an array of "dictionary"-hashmaps. dictionaries[N] is a dictionary with
+   *                     N-char-length keys.
+   * @return the real symbol length, or 1 if the symbol cant be found in given dictionaries.
+   */
+  private int detectSymbolLength(String inputString, int i, int expectedSymbolLength,
+          HashMap[] dictionaries){
+    try {
+      String symbol = inputString.substring(i,i+expectedSymbolLength);
+      if (expectedSymbolLength==1) {
+        //symbol not found =>(punctuation/space? can't translate)
+        return 1;
+      }
+      if (dictionaries[expectedSymbolLength].containsKey(symbol)) {
+        return expectedSymbolLength;
+      }
+      //if neither
+      throw new IllegalArgumentException();
+      //IllegalArg / ArrayOutOf
+    } catch (Exception e){
+        return detectSymbolLength(inputString, i, expectedSymbolLength-1,dictionaries );
+    }
+  }
+
+  private String nextSymbol(String inputString, int i, int symbolLength,
+          HashMap[] dictionaries) {
+    String symbol = inputString.substring(i,i+symbolLength);
+    return (String) dictionaries[symbolLength].get(symbol);
+  }
 }
